4bbf19d3b329359fc1bd04de5d8aa920cfceb45a
Nadia <nlnadialigia@gmail.com>
2021-03-14 00:56:26 -0300

    refactor: fix bug in .env

--- server/infra/database.js
+++ server/infra/database.js
@@ -1,10 +1,10 @@
-const dotenv = require('dotenv');
 const pgp = require('pg-promise')();
+const dotenv = require('dotenv');
 
 dotenv.config();
 
 const db = pgp({
-  user: process.env.USER,
+  user: process.env.USERDB,
   password: process.env.PASSWORD,
   host: process.env.HOST,
   port: process.env.PORT,
